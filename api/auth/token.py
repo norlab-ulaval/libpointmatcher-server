a0cb836f701bc486de59f68b7323d5cb8454b641
ThierryCrispin <thierry.crispin@gmail.com>
2024-02-15 20:08:11 -0500

    Storing tokens with redis

--- api/auth/token.py
+++ api/auth/token.py
@@ -1,9 +1,13 @@
+import redis
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
+from interface.interface_models import TokenData
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 SECRET_KEY = "faudrait générer une clé"
 ALGORITHM = "HS256"
+
+redis_client = redis.Redis(host="cache", port=6379, db=0, password="potato123")
 
 def create_access_token(user) -> str:
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -13,4 +17,22 @@ def create_access_token(user) -> str:
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    redis_client.set(user.username, encoded_jwt, ex=access_token_expires)
+
     return encoded_jwt
+
+def get_validated_tokenData(token: str) -> TokenData | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+        
+        token = redis_client.get(username)
+        if token is None:
+            return None
+
+        return TokenData(username=username)
+    
+    except JWTError:
+        return None
