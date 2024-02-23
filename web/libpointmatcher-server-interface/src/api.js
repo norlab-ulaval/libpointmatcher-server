42707f0ea879d678473125b2e0260975867f6d9a
ThierryCrispin <70489877+ThierryCrispin@users.noreply.github.com>
2024-02-23 14:24:58 -0500

    Leaderboard frontend function (#63)
    * getLeaderboard function. fix error message
    
    * oops. wrong attribute.

--- web/libpointmatcher-server-interface/src/api.js
+++ web/libpointmatcher-server-interface/src/api.js
@@ -39,7 +39,7 @@ export const login = async (email, password) => {
     const jsonResponse = await response.json();
 
     if (response.ok) {
-      Cookies.set("token", jsonResponse.token, { expires: 1 });
+      Cookies.set("token", jsonResponse.access_token, { expires: 1 }, { secure: true });
       return { success: true };
     } else {
       return { success: false, error: jsonResponse.detail || "Login failed"};
@@ -52,4 +52,31 @@ export const login = async (email, password) => {
 export const logout = async () => {
     // À modifier
     Cookies.remove("token");
-  };
+};
+
+export const getLeaderboard = async (page, limit, type) => {
+  const token = Cookies.get("token");
+  
+  try {
+    const request = new Request(`${endpoint}/leaderboard`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      body: JSON.stringify({ page: page, limit: limit, type: type }),
+    });
+
+    const response = await fetch(request);
+    const jsonResponse = await response.json();
+
+    if (response.ok) {
+      return { success: true, leaderboard: jsonResponse };
+    }
+    else {
+      return { success: false, error: jsonResponse.detail || "Failed to fetch leaderboard." };
+    }
+  } catch (error) {
+    return { success: false, error: "Network error or server is unreachable." };
+  }
+}
