c58a9efc8d1d43d16c12576fec19b6408cee28ab
Gabriel Gagnon <gabriel.gagnon.14@ulaval.ca>
2024-02-29 11:53:30 -0500

    fix tests

--- api/test_e2e.py
+++ api/test_e2e.py
@@ -1,19 +1,31 @@
+import os
 import pytest
+from fastapi.testclient import TestClient
 from httpx import Response, AsyncClient
+from motor.core import AgnosticCollection
 
+from db.mongo import get_database
+
+from main import app
+
+TEST_USER = 'test_user'
+client = TestClient(app)
+
+
+
+@pytest.fixture(autouse=True)
+async def run_around_tests():
+    await remove_test_user()
 
 @pytest.mark.anyio
 async def test_hello_world(client: AsyncClient):
     response = await client.get('/')
     assert response.status_code == 200
 
-
 @pytest.mark.anyio
 async def test_register_then_login_with_success(client: AsyncClient):
-    # TODO Delete user if exists before
-
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'DCL2D7zi6y8Q7a6Ib!'
     }
@@ -34,7 +46,8 @@ async def test_register_then_login_with_success(client: AsyncClient):
     if login_response.status_code != 200:
         print(vars(login_response))
         raise AssertionError
-    
+
+
 @pytest.mark.anyio
 async def test_register_with_too_short_username(client: AsyncClient):
     user_data = {
@@ -49,10 +62,11 @@ async def test_register_with_too_short_username(client: AsyncClient):
         print(vars(register_response))
         raise AssertionError
 
+
 @pytest.mark.anyio
 async def test_register_with_bad_email(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example',
         'password': 'DCL2D7zi6y8Q7a6Ib!'
     }
@@ -62,11 +76,12 @@ async def test_register_with_bad_email(client: AsyncClient):
     if register_response.status_code != 400:
         print(vars(register_response))
         raise AssertionError
-    
+
+
 @pytest.mark.anyio
 async def test_register_with_too_short_password(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'D2D7z!'
     }
@@ -77,10 +92,11 @@ async def test_register_with_too_short_password(client: AsyncClient):
         print(vars(register_response))
         raise AssertionError
 
+
 @pytest.mark.anyio
 async def test_register_with_missing_upper_password(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'abcde12345!'
     }
@@ -91,10 +107,11 @@ async def test_register_with_missing_upper_password(client: AsyncClient):
         print(vars(register_response))
         raise AssertionError
 
+
 @pytest.mark.anyio
 async def test_register_with_missing_lower_password(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'ABCDE12345!'
     }
@@ -104,11 +121,12 @@ async def test_register_with_missing_lower_password(client: AsyncClient):
     if register_response.status_code != 400:
         print(vars(register_response))
         raise AssertionError
-    
+
+
 @pytest.mark.anyio
 async def test_register_with_no_symbols_password(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'DCL2D7zi6y8Q7a6Ib'
     }
@@ -118,11 +136,12 @@ async def test_register_with_no_symbols_password(client: AsyncClient):
     if register_response.status_code != 400:
         print(vars(register_response))
         raise AssertionError
-    
+
+
 @pytest.mark.anyio
 async def test_register_then_try_login_with_wrong_email(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'DCL2D7zi6y8Q7a6Ib!'
     }
@@ -143,11 +162,12 @@ async def test_register_then_try_login_with_wrong_email(client: AsyncClient):
     if login_response.status_code != 401:
         print(vars(login_response))
         raise AssertionError
-    
+
+
 @pytest.mark.anyio
 async def test_register_then_try_login_with_wrong_password(client: AsyncClient):
     user_data = {
-        'username': 'test_user',
+        'username': TEST_USER,
         'email': 'test@example.com',
         'password': 'DCL2D7zi6y8Q7a6Ib!'
     }
@@ -168,3 +188,12 @@ async def test_register_then_try_login_with_wrong_password(client: AsyncClient):
     if login_response.status_code != 401:
         print(vars(login_response))
         raise AssertionError
+
+
+async def remove_test_user():
+    env = os.environ
+    db = get_database(env)
+    users_collection: AgnosticCollection = db['users']
+    user = users_collection.find_one({"username": TEST_USER})
+    if user:
+        await users_collection.delete_one({'username': TEST_USER})
