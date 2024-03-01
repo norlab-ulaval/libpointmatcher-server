c8180e86dddc44ae1826264cd328009fa6883e70
ThierryCrispin <70489877+ThierryCrispin@users.noreply.github.com>
2024-03-01 12:39:47 -0500

    Finalisation logout (#67)
    * fonctions logout
    
    * function changes

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 
-from interface.interface_models import Token, RegisteringUser, LoginUser
+from auth.auth import get_current_user
+from interface.interface_models import Token, RegisteringUser, User
 from user.user_controller import UserController
 
 user_controller: UserController
@@ -20,3 +21,7 @@ async def register_user(user: RegisteringUser):
 @router.post("/login", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     return await user_controller.authenticate(form_data.username, form_data.password)
+
+@router.post("/logout")
+async def logout_user(user: Annotated[User, Depends(get_current_user)]):
+    return await user_controller.logout(user)
