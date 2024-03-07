a8fb03a8a740b1a27e5dbe4ab3136467dc0b342d
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2024-03-07 15:47:50 -0500

    Add initial navbar

--- web/libpointmatcher-server-interface/src/App.vue
+++ web/libpointmatcher-server-interface/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <Navbar v-if="$route.name !== 'auth'" />
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/layout/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  }
+};
+</script>
---
+++ web/libpointmatcher-server-interface/src/components/layout/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="bg-white shadow">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+            <div>
+                <img src="/public/norlab_logo_acronym_light.png" alt="logo" class="h-24 w-24">
+            </div>
+
+            <div class="flex">
+                <a class="text-gray-700 hover:text-blue-500 mx-4" href="#">Home</a>
+                <a class="text-gray-700 hover:text-blue-500 mx-4" href="#">Uploads</a>
+                <a class="text-gray-700 hover:text-blue-500 mx-4" href="#">Profile</a>
+            </div>
+
+            <div>
+                <a class="text-gray-700 hover:text-blue-500 mx-4" href="#">Log out</a>
+            </div>
+        </div>
+    </nav>
+</template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
