db34e2b13f16415669ddfe6182839d40bdd844d7
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2024-03-07 21:25:15 -0500

    Add home router link to logo

--- web/libpointmatcher-server-interface/src/components/layout/Navbar.vue
+++ web/libpointmatcher-server-interface/src/components/layout/Navbar.vue
@@ -3,28 +3,28 @@
         <div class="flex items-center justify-between px-10 py-2 mx-auto">
 
             <div class="pl-8">
-                <router-link to="/home">
+                <router-link to="/home" class="inline-block p-0 m-0 align-middle">
                     <img src="/public/norlab_logo_acronym_light.png" alt="logo" class="h-auto" style="width: 11rem;">
-                </router-link>                
+                </router-link>                    
             </div>
 
             <div class="flex items-center mr-auto ml-8">
                 <router-link
-                    class="text-white border-b-3 border-transparent hover:border-white custom-text-size mr-8 pb-2 font-sans"
+                    class="text-white border-b-2 border-transparent hover:border-white custom-text-size mr-8 pb-2 font-sans font-light"
                     to="/home"
                     active-class="border-white"
                 >
                     Home
                 </router-link>
                 <router-link
-                    class="text-white border-b-3 border-transparent hover:border-white custom-text-size mr-8 pb-2 font-sans"
+                    class="text-white border-b-2 border-transparent hover:border-white custom-text-size mr-8 pb-2 font-sans font-light"
                     to="/uploads"
                     active-class="border-white"
                 >
                     Uploads
                 </router-link>
                 <router-link
-                    class="text-white border-b-3 border-transparent hover:border-white custom-text-size pb-2 font-sans"
+                    class="text-white border-b-2 border-transparent hover:border-white custom-text-size pb-2 font-sans font-light"
                     to="/profile"
                     active-class="border-white"
                 >
@@ -33,7 +33,7 @@
             </div>
 
             <div class="pr-8">
-                <a class="text-white border-b-3 border-transparent hover:border-white custom-text-size font-sans pb-2" @click="handleLogout">Log out</a>
+                <a class="text-white border-b-2 border-transparent hover:border-white custom-text-size font-sans pb-2 cursor-pointer font-light" @click="handleLogout">Log out</a>
             </div>
         </div>
     </nav>
@@ -59,7 +59,7 @@
 
 <style>
 .custom-text-size {
-    font-size: 1.5rem;
+    font-size: 1.65rem;
 }
 </style>
   
