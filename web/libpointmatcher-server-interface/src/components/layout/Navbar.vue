9735567203f786613d574554eb58d80ec7aa0b2c
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2024-03-07 20:44:59 -0500

    Add router link and hover action

--- web/libpointmatcher-server-interface/src/components/layout/Navbar.vue
+++ web/libpointmatcher-server-interface/src/components/layout/Navbar.vue
@@ -3,17 +3,37 @@
         <div class="flex items-center justify-between px-10 py-2 mx-auto">
 
             <div class="pl-8">
-                <img src="/public/norlab_logo_acronym_light.png" alt="logo" class="h-auto" style="width: 11rem;">
+                <router-link to="/home">
+                    <img src="/public/norlab_logo_acronym_light.png" alt="logo" class="h-auto" style="width: 11rem;">
+                </router-link>                
             </div>
 
-            <div class="flex items-center mr-auto ml-8"> 
-                <a class="text-white hover:text-blue-500 text-xl mr-8 font-mono" href="#">HOME</a>
-                <a class="text-white hover:text-blue-500 text-xl mr-8 font-mono" href="#">UPLOADS</a>
-                <a class="text-white hover:text-blue-500 text-xl font-mono" href="#">PROFILE</a>
+            <div class="flex items-center mr-auto ml-8">
+                <router-link
+                    class="text-white border-b-3 border-transparent hover:border-white custom-text-size mr-8 pb-2 font-sans"
+                    to="/home"
+                    active-class="border-white"
+                >
+                    Home
+                </router-link>
+                <router-link
+                    class="text-white border-b-3 border-transparent hover:border-white custom-text-size mr-8 pb-2 font-sans"
+                    to="/uploads"
+                    active-class="border-white"
+                >
+                    Uploads
+                </router-link>
+                <router-link
+                    class="text-white border-b-3 border-transparent hover:border-white custom-text-size pb-2 font-sans"
+                    to="/profile"
+                    active-class="border-white"
+                >
+                    Profile
+                </router-link> 
             </div>
 
             <div class="pr-8">
-                <a class="text-white hover:text-blue-500 text-xl font-mono" href="#">LOG OUT</a>
+                <a class="text-white border-b-3 border-transparent hover:border-white custom-text-size font-sans pb-2" href="#">Log out</a>
             </div>
         </div>
     </nav>
@@ -24,4 +44,10 @@
     name: 'Navbar',
   };
   </script>
+
+<style>
+.custom-text-size {
+    font-size: 1.5rem;
+}
+</style>
   
