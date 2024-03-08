870ca0e05048f51af60c12b5586f0321d4850ab8
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2024-03-07 23:18:59 -0500

    Add search glass svg

--- web/libpointmatcher-server-interface/src/components/leaderboard/LeaderboardTable.vue
+++ web/libpointmatcher-server-interface/src/components/leaderboard/LeaderboardTable.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="container mx-auto px-4 py-8">
       <h1 class="text-3xl font-bold text-center mb-4">libpointmatcher Leaderboard</h1>
-      <p class="text-center mb-8">Discover the most effective configurations contributed by our community.</p>
+      <p class="text-center mb-16">Discover the most effective configurations contributed by our community.</p>
       
-      <div class="mb-4">
+      <div class="mb-4 relative">
+        <svg class="w-6 h-6 text-gray-800 dark:text-white absolute left-3 top-1/2 transform -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+        </svg>
         <input
           type="text"
-          class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          class="border-2 border-gray-300 bg-white h-10 pl-12 pr-5 rounded-lg text-sm focus:outline-none"
           placeholder="Search..."
           v-model="searchQuery"
         />
