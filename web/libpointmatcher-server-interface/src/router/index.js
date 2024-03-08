5f062e54d25c4d21c7c7e58504ac7eef2a6cd956
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2024-03-07 22:18:16 -0500

    Remove auto generated components

--- web/libpointmatcher-server-interface/src/router/index.js
+++ web/libpointmatcher-server-interface/src/router/index.js
@@ -7,21 +7,18 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/auth',
+      path: '/',
       name: 'auth',
       component: AuthView,
     },
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: About
     }
   ]
