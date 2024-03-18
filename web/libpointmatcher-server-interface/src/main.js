b067c69a7bac1fcf870df91d49951504163d30f4
ThierryCrispin <thierry.crispin@gmail.com>
2024-03-17 23:05:11 -0400

    3d test working

--- web/libpointmatcher-server-interface/src/main.js
+++ web/libpointmatcher-server-interface/src/main.js
@@ -6,10 +6,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VuePapaParse from 'vue-papa-parse'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VuePapaParse)
 
 app.mount('#app')
