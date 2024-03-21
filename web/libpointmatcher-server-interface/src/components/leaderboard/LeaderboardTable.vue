cd03684de036a5df0b419ca8e7adc0a4cadd94f9
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2024-03-21 12:02:57 -0400

    center pagination

--- web/libpointmatcher-server-interface/src/components/leaderboard/LeaderboardTable.vue
+++ web/libpointmatcher-server-interface/src/components/leaderboard/LeaderboardTable.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto px-4 py-8">
+    <div class="container mx-auto px-4 pt-8 pb-2">
       <h1 class="text-3xl font-bold text-center mb-4">libpointmatcher Leaderboard</h1>
       <p class="text-center mb-8">Discover the most effective configurations contributed by our community.</p>
       
@@ -45,6 +45,30 @@
           </tbody>
         </table>
       </div>
+      <nav class="flex justify-center items-center gap-x-1 mt-2">
+        <button type="button"
+          @click="changePage(currentPage - 1)"
+          :disabled="currentPage === 1"
+          class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+        >
+          <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+          <span aria-hidden="true" class="sr-only">Previous</span>
+        </button>
+        <div class="flex items-center gap-x-1">
+          <span class="min-h-[38px] min-w-[38px] flex justify-center items-center border border-gray-200 text-gray-800 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:text-white dark:focus:bg-white/10">1</span>
+          <span class="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm dark:text-gray-500">of</span>
+          <span class="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm dark:text-gray-500">3</span>
+        </div>
+        <button 
+          type="button"
+          @click="changePage(currentPage + 1)"
+          :disabled="currentPage === totalPages"
+          class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+        >
+          <span aria-hidden="true" class="sr-only">Next</span>
+          <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+        </button>
+      </nav>
     </div>
   </template>
   
@@ -66,7 +90,9 @@
             { date: '2023-04-02', version: '1.8.1', name: 'John doe', score: 92, type: 'Average' },
             { date: '2023-03-14', version: '1.3.7', name: 'Delta research', score: 89, type: 'Average' },
             { date: '2023-03-14', version: '1.3.7', name: 'Delta research', score: 89, type: 'Average' }
-        ]
+        ],
+        currentPage: 1,
+        totalPages: 0,
       };
     },
     computed: {
