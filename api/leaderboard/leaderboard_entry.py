6e8a74381afefb566d271ea5051845615ae659f3
antoinegagnon9 <97764959+antoinegagnon9@users.noreply.github.com>
2024-03-23 19:14:10 -0400

    implemented mongo requests for leaderboard (#101)

--- api/leaderboard/leaderboard_entry.py
+++ api/leaderboard/leaderboard_entry.py
@@ -1,12 +1,12 @@
 class LeaderboardEntry:
-    username = ""
+    email = ""
     score = ""
     score_type = ""
     version = ""
     date = ""
 
-    def __init__(self, username=None, score=None, score_type=None, version=None, date=None):
-        self.username = username
+    def __init__(self, email=None, score=None, score_type=None, version=None, date=None):
+        self.email = email
         self.score = score
         self.score_type = score_type
         self.version = version
