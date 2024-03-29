2ed4f4fded341885e6d1f683f9f7a1455352aa70
ThierryCrispin <thierry.crispin@gmail.com>
2024-03-28 20:49:39 -0400

    Removing 3d from HomeView

--- web/libpointmatcher-server-interface/src/views/HomeView.vue
+++ web/libpointmatcher-server-interface/src/views/HomeView.vue
@@ -3,18 +3,6 @@
   <div class="flex items-center justify-center">
     <div>
       <h1> Welcome page </h1>
-      <PointsVisualizer />
     </div>
   </div>
 </template>
-
-<script>
-import PointsVisualizer from '@/components/3d/PointsVisualizer.vue';
-
-export default {
-  name: 'HomeView',
-  components: {
-    PointsVisualizer
-  }
-};
-</script>
