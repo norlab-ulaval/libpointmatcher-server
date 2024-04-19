bf5ec12f0d7f1215bb970a913e9d04e395dd0413
Gabriel Gagnon <gabriel.gagnon97@gmail.com>
2024-04-19 15:10:46 -0400

    using deque and added some exception handling

--- api/adapter/libpointmatcher_adapter.py
+++ api/adapter/libpointmatcher_adapter.py
@@ -1,31 +1,37 @@
 import json
-import uuid
 from time import sleep
-
+from collections import deque
 from websockets.sync.client import connect
 from os import environ as env
-
 from evaluation.evaluator import Evaluator
+from uuid import uuid4
 
-queue = []
+queue = deque()
 
 
 class LibpointmatcherAdapter(Evaluator):
     def evaluate_config(self, config: str) -> dict[str, dict[str, dict[str, list]]]:
-        ticket = uuid.UUID()
+        ticket = str(uuid4())
         queue.append(ticket)
 
         while queue[0] != ticket:
             sleep(1)
 
-        result = self.evaluate(config)
+        try:
+            result = self.evaluate(config)
+        except Exception as e:
+            queue.popleft()
+            raise e
 
-        queue.pop()
+        queue.popleft()
 
         return result
 
     def evaluate(self, config: str) -> dict[str, dict[str, dict[str, list]]]:
-        with connect(f"ws://{env.get('LIBPOINTMATCHER_WS', 'localhost:8765')}") as websocket:
-            websocket.send(config)
-            results = websocket.recv()
-            return json.loads(results)
+        try:
+            with connect(f"ws://{env.get('LIBPOINTMATCHER_WS', 'localhost:8765')}") as websocket:
+                websocket.send(config)
+                results = websocket.recv()
+                return json.loads(results)
+        except Exception as e:
+            reau
